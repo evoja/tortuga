700ec1a7e9cdd4f4fd7c68af8bf8086c7aa3435b
chivorotkiv <shmakov@omich.net>
2013-02-18 17:08:25 +0700

    Черепашка, которая умеет ходить и поворачивать. Всегда рисует.

--- js-tortoise-project/js/tortoise.js
+++ js-tortoise-project/js/tortoise.js
@@ -1,4 +1,64 @@
-var initTortoise = function()
+var createTortoise;
+var initTortoise = function(canvas, tortoiseContainer)
 {
+	var width = 32;
+	var height = 32;
+	createTortoise = function(xx, yy)
+	{
+		var ttd = document.createElement("DIV");
+		ttd.className = "ttx";
+		ttd.style.width = width + "px";
+		ttd.style.height = height + "px";
+		tortoiseContainer.appendChild(ttd);
 
+		var x = xx;
+		var y = yy;
+		var color = "#000";
+		var rotation = 180;
+
+		var radRot = function()
+		{
+			return Math.PI - degToRad(rotation);
+		}
+
+		var updateDiv = function()
+		{
+			var rad = radRot();
+			var dx = width * (Math.cos(rad)/2 + Math.sin(rad));
+			var dy = height * (Math.cos(rad) - Math.sin(rad) / 2);
+			ttd.style.left = (x + dx) + "px";
+			ttd.style.top =  (y + dy) + "px";
+			ttd.style["-webkit-transform"] = "rotate(" + rotation + "deg)";
+			ttd.style["-webkit-transform-origin"] = "0% 0%"
+		}
+
+		var degToRad = function(deg)
+		{
+			return deg / 180 * Math.PI;
+		}
+
+		var tortoise = {
+			go: function(length)
+			{
+				var ox = x;
+				var oy = y;
+
+				var rad = radRot();
+				x += length * Math.sin(rad);
+				y += length * Math.cos(rad);
+				updateDiv();
+
+				oldColor = setColor(color);
+				drawLine(ox, oy, x, y);
+				setColor(oldColor);
+			},
+			rotate: function(deg)
+			{
+				rotation -= deg;
+				updateDiv();
+			}
+		}
+		updateDiv();
+		return tortoise;
+	}
 }
---
+++ js-tortoise-project/proga.js
@@ -0,0 +1,4 @@
+setColor("#0f0");
+drawPoint(100, 100);
+drawPoint(100, 200);
+drawPoint(300, 200);
