1d6d07416351649412a5f6c72897498a73dc9e52
chivorotkiv <shmakov@omich.net>
2013-02-18 15:42:26 +0700

    Выношу скрипты из HTML в отдельные JS файлики

---
+++ js-tortoise-project/js/drawing.js
@@ -0,0 +1,43 @@
+var drawLine;
+var drawCircle;
+var setColor;
+
+var init = function()
+{
+	var ctx = document.getElementById("ctx").getContext("2d");
+	var pointSize = 5;
+
+	drawLine = function (x, y, x1, y1)
+	{
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.lineTo(x1, y1);
+		ctx.closePath();
+		ctx.stroke();
+	}
+
+	drawPoint = function(x, y)
+	{
+		drawCircle(x, y, pointSize);
+	}
+
+	drawCircle = function(x, y, r)
+	{
+		ctx.beginPath();
+		ctx.arc(x,y,r,0,2*Math.PI);
+		ctx.closePath();
+		ctx.stroke();
+	}
+
+	setColor = function(color)
+	{
+		ctx.strokeStyle = color;
+	}
+
+	setPointRadius = function(r)
+	{
+		pointSize = r;
+	}
+
+	initFiles(	document.getElementById('files'));
+}
