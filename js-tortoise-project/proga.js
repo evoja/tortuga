c5587a87be5c67dd5507b175cea5e8faf80819b4
chivorotkiv <shmakov@omich.net>
2013-02-18 23:16:06 +0700

    Ссылки на видеоинструкции для Савченко.

--- js-tortoise-project/proga.js
+++ js-tortoise-project/proga.js
@@ -1,4 +1,50 @@
-setColor("#0f0");
-drawPoint(100, 100);
-drawPoint(100, 200);
-drawPoint(300, 200);
+(function()
+{
+	var st;
+	var isDrawing;
+
+	selectTortoise = function(t)
+	{
+		st = t
+		st.tailUp();
+		isDrawing = false;
+	}
+
+	document.onkeydown = function(e)
+	{
+		e = e || window.event;
+		var code = event.keyCode;
+	    if (event.charCode && code == 0)
+	          code = event.charCode;
+	     switch(code) {
+	          case 37:
+	              // Key left.
+	              st.rotate(15);
+	              break;
+	          case 38:
+	              // Key up.
+	              st.go(2);
+	              break;
+	          case 39:
+	              // Key right.
+	              st.rotate(-15);
+	              break;
+	          case 40:
+	              // Key down.
+	              if(isDrawing)
+	              {
+	              	st.tailUp();
+	              }
+	              else
+	              {
+	              	st.tailDown();
+	              }
+	              isDrawing = !isDrawing;
+	              break;
+	          default:
+	          	return;
+	     }
+	     event.preventDefault();
+	}
+})()
+
