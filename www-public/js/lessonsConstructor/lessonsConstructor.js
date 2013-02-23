2c61c782e45a200d8c56cd76167b21b0f4a53c6d
chivorotkiv <shmakov@omich.net>
2013-02-23 23:54:51 +0700

    текстовое поле с адресом в конструкторе перестало быть ссылкой. ссылку добавляю рядом.

--- www-public/js/lessonsConstructor/lessonsConstructor.js
+++ www-public/js/lessonsConstructor/lessonsConstructor.js
@@ -4,26 +4,33 @@ var initLessonConstructor;
 var getLinkAreaText = function(areaValue)
 {
 	var link = ParamsUtil.getLessonLink(areaValue);
-	return link.length > 2000 ? areaValue.length : link;
+	return link;
 }
 
 var updateLinkArea = function(linkarea, areaValue)
 {
 	var url = getLinkAreaText(areaValue);
 
-	var textinput = document.createElement("INPUT");
-	textinput.type = "text";
-	textinput.disabled = true;
-	textinput.style.width = "100%";
-	textinput.value = url;//'<a href="' + url + '">' + url + '</a>';
-
-	var link = document.createElement("A");
-	link.href = url;
-	link.appendChild(textinput);
 	linkarea.innerHTML = "";
-	linkarea.appendChild(link);
+	if(url.length < 2000)
+	{
+		var textinput = document.createElement("INPUT");
+		textinput.type = "text";
+		textinput.disabled = true;
+		textinput.value = url;
 
-	textinput.select();
+		var link = document.createElement("A");
+		link.href = url;
+		link.innerHTML = "Try lesson";
+
+		linkarea.appendChild(textinput);
+		linkarea.appendChild(link);
+		textinput.select();
+	}
+	else
+	{
+		linkarea.innerHTML = "Слишком длинный урок (" + areaValue.length + ")";
+	}
 }
 
 initLessonConstructor = function(area, button, linkarea)
