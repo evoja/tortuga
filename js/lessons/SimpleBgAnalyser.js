c43f1a76b7ffd9ac053fbbfeaccac726542d82ff
chivorotkiv <shmakov@omich.net>
2013-05-11 11:10:42 +0700

    Убрал сайт с ГитХабовской страницы. Зато поставил туда переадресацию на новый сайт http://www.trtg.org

--- js/lessons/SimpleBgAnalyser.js
+++
@@ -1,7 +0,0 @@
-ns("Tortuga");
-Tortuga.initTortoiseCanvasBackground = function(div)
-{
-	var lesson = ParamsUtil.getLesson()[0];
-	document.title = lesson.title;
-	div.style["background-image"] = 'url("' + lesson.src + '")';
-}
