cff694ee8b40be09ad8d23a5b45c5320fbb513e8
Evgen Prit <pritevgeniy@gmai.com>
2013-11-11 13:55:37 +0700

    [d-83] Вынесены локальные функции

--- www-public/js/lessons/lessons.js
+++ www-public/js/lessons/lessons.js
@@ -156,6 +156,23 @@ LessonEnv.prototype.setLessonsTitle = function(itemTitle)
 	this.tortugaEnv.setLessonsTitle(itemTitle + " \\ " + this.title);
 }
 
+var buildListOfTabs = function(bg, list, descrDiv, env, lesson)
+{
+	list.appendChild(createList(lesson.items, bg, descrDiv,
+	new LessonEnv(env, lesson.title))); 
+}
+
+var replacementDOMListOfTabs = function(bg, list, descrDiv, env, lesson)
+{
+	for(var i=1; i<=list.children.length; i++) 
+	{
+    	var child = list.children[i];
+   		list.removeChild(child);
+	}
+
+	buildListOfTabs(bg, list, descrDiv, env, lesson);
+}
+
 Tortuga.initLessons = function(bg, list, descrDiv, env, allContainers)
 {
 	var lesson = Tortuga.ParamsUtil.getLesson();
@@ -182,21 +199,7 @@ Tortuga.initLessons = function(bg, list, descrDiv, env, allContainers)
 	}
 
 
- var buildListOfTabs = function(bg, list, descrDiv, env, lesson)
- {
- 	list.appendChild(createList(lesson.items, bg, descrDiv,
-	new LessonEnv(env, lesson.title))); 
- }
 
- var replacementDOMListOfTabs = function(bg, list, descrDiv, env, lesson)
- {
- 	for(var i=1; i<=list.children.length; i++) {
-        var child = list.children[i];
-        list.removeChild(child);
-	}
-
-	buildListOfTabs(bg, list, descrDiv, env, lesson);
- }
 	
 	if ("onhashchange" in window)
 	{
