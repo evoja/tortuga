585287c64086ce34f340779b413ecaa65c87166e
Evgen Prit <pritevgeniy@gmai.com>
2014-02-12 12:31:29 +0700

    [d-119] пишет в консоль событие, которое произошло

--- www-public/js/mouse.js
+++ www-public/js/mouse.js
@@ -19,5 +19,22 @@ ns("Tortuga.Events");
 				Tortuga.Events.onclick(event_canvas_onclick);
 			}
 		}
+
+		drawingSystem.getCanvas().onmousedown = function(e){console.log("onmousedown")}
+
+		drawingSystem.getCanvas().onmouseenter = function(e){console.log("onmouseenter")}
+
+		drawingSystem.getCanvas().onmouseleave = function(e){console.log("onmouseleave")}
+
+		drawingSystem.getCanvas().onmousemove = function(e){console.log("onmousemove")}
+
+		drawingSystem.getCanvas().onmouseout = function(e){console.log("onmouseout")}
+
+		drawingSystem.getCanvas().onmouseover = function(e){console.log("onmouseover")}
+
+		drawingSystem.getCanvas().onmouseup = function(e){console.log("onmouseup")}
+
+		drawingSystem.getCanvas().onmousewheel = function(e){console.log("onmousewheel")}
+
 	}
 })()
