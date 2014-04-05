c35d0a405ecd63e745218fc7d51d2db661bac495
chivorotkiv <shmakov@omich.net>
2014-04-05 15:10:28 +0700

    [d-134] Можно играть в консоли!

--- examples/perepravy_shapovalov.js
+++ examples/perepravy_shapovalov.js
@@ -10,21 +10,33 @@
 
 
 // CI means index
-var CI_TYPE = 0;
-var CI_FAMILY = 1;
+var CI_TYPE = 0
+var CI_FAMILY = 1
 
 var slice = Array.prototype.slice
 var bind = Function.prototype.bind
 
-var test_count = 0;
+var test_count = 0
 
 var assert = function(bool, message)
 {
 	if(!bool)
 	{
-		console.log("Test #" + test_count + " failed: ", message);
+		message = typeof message == "function" ? message() : message
+		console.log("Test #" + test_count + " failed: ", message)
 	}
-	++test_count;
+	++test_count
+}
+
+var map = function(arr, fun)
+{
+	var result = []
+	var arr_len = arr.length
+	for(var i = 0; i < arr_len; ++i)
+	{
+		result.push(fun(arr[i], i))
+	}
+	return result
 }
 
 
@@ -514,7 +526,7 @@ var required = function(first, second)
 var POS_RIGHT = "right";
 var POS_LEFT = "left";
 
-(function()
+var game_contructors = (function()
 {
 	var select_boat_position = function(left, right)
 	{
@@ -600,6 +612,9 @@ var POS_LEFT = "left";
 		var what_len = what.length
 		for(var i = 0; i < what_len; ++i)
 		{
+			if(!what[i])
+				continue
+
 			var j = find_obj_in_arr(rest, what[i])
 
 			if(j != -1 && get_transaction_rule(transaction_rules, what[i])(all))
@@ -666,7 +681,12 @@ var POS_LEFT = "left";
 		return true
 	}
 
-	var Game = function(cfg)
+	var everybody_on_the_right = function(game)
+	{
+		return game.left.length == 0 && game.boat.length == 0
+	}
+
+	var GameRaw = function(cfg)
 	{
 		this.left = cfg.left || []
 		this.right = cfg.right || []
@@ -679,11 +699,12 @@ var POS_LEFT = "left";
 			boat_moving_rules: cfg.boat_moving_rules || not_empty,
 			boat_capacity: cfg.boat_capacity || 0,
 			types_weights: cfg.types_weights || [],
-			transaction_rules: cfg.transaction_rules || true_fun
+			transaction_rules: cfg.transaction_rules || true_fun,
+			win_rules: cfg.win_rules || everybody_on_the_right
 		}
 	}
 
-	Game.prototype.to_right = function(what)
+	GameRaw.prototype.to_right = function(what)
 	{
 		var game = this
 		return do_transaction(game, game.left, game.right, what,
@@ -691,7 +712,7 @@ var POS_LEFT = "left";
 			game.config.left_rules, game.config.right_rules)
 	}
 
-	Game.prototype.to_left = function(what)
+	GameRaw.prototype.to_left = function(what)
 	{
 		var game = this
 		return do_transaction(game, game.right, game.left, what,
@@ -699,8 +720,27 @@ var POS_LEFT = "left";
 			game.config.right_rules, game.config.left_rules)
 	};
 
+	GameRaw.prototype.is_win = function()
+	{
+		return this.config.win_rules(this)
+	};
+
+	(function test_custom_win(){
+		var game = new GameRaw({
+			left: [["muzhik"], ["muzhik"], ["muzhik"]],
+			boat_capacity: 3,
+			win_rules: function(game){game.left.length == 2}
+		});
+
+		assert(!game.is_win(), "Not win")
+		game.to_right([["muzhik"], ["muzhik"]])
+		assert(!game.is_win(), "Not win")
+		game.to_left([["muzhik"]])
+		assert(game.is_win(), "Win")
+	});
+
 	(function test_game_stiralka(){
-		var game = new Game({
+		var game = new GameRaw({
 			left: [["muzhik"], ["muzhik"], ["muzhik"], ["stiralka"]],
 			boat_capacity: 3,
 			boat_rules: necessary(["muzhik"]),
@@ -708,10 +748,13 @@ var POS_LEFT = "left";
 		});
 
 		assert(game.to_right([["muzhik"], ["muzhik"], ["stiralka"]]), "correct: [muzhik] --- [stiralka]-[muzhik, muzhik]")
+		assert(!game.is_win(), "Not win")
 		assert(!game.to_right([["muzhik"]]), "Incorrect boat side")
 		assert(!game.to_left([]), "Can't go without muzhik")
 		assert(game.to_left([["muzhik"]]), "correct: [muzhik, muzhik]-[stiralka] --- [muzhik]")
+		assert(!game.is_win(), "Not win")
 		assert(game.to_right([["muzhik"], ["muzhik"]]), "correct: [] --- [] - [muzhik, muzhik, muzhik, stiralka]")
+		assert(game.is_win(), "Win")
 	})();
 
 	(function test_game_volk_koza_kapusta(){
@@ -725,39 +768,486 @@ var POS_LEFT = "left";
 		var muzhik_koza = needs(koza, muzhik)
 		var muzhik_kapusta = needs(kapusta, muzhik)
 
-		var game = new Game({
+		var game = new GameRaw({
 			left: [volk, koza, kapusta, muzhik],
 			boat_capacity: 2,
 			rules: or(necessary(muzhik), and(koza_volk, koza_kapusta)),
 			boat_rules: necessary(muzhik)
 		});
 
+		assert(!game.is_win(), "Not win")
 		assert(!game.to_right([volk]), "Can't go without muzhik")
 		assert(!game.to_right([muzhik, kapusta]), "Can't go: volk eats koza")
 		assert(!game.to_right([muzhik, volk]), "Can't go: koza eats kapusta")
 		assert(game.to_right([muzhik, koza]), "Correct: [volk, kapusta] --- []-[muzhik, koza]")
+		assert(!game.is_win(), "Not win")
 		assert(!game.to_left([koza]), "Can't go without muzhik")
 		assert(game.to_left([muzhik, volk]), "There are no volk but muzhik moves: [volk, kapusta, muzhik]-[] --- [koza]")
+		assert(!game.is_win(), "Not win")
 		assert(!game.to_left([muzhik]), "Boat is already on the left")
 		assert(!game.to_right([muzhik, volk, kapusta]), "Overweight!")
 		assert(!game.to_right([volk, kapusta]), "Can't go without muzhik")
 		assert(game.to_right([muzhik, kapusta]), "Correct: [volk] --- []-[muzhik, koza, kapusta]")
+		assert(!game.is_win(), "Not win")
 		assert(!game.to_left([muzhik]), "Can't go: koza eats kapusta")
 		assert(game.to_left([muzhik, koza]), "Correct: [volk, muzhik, koza]-[] --- [kapusta]")
+		assert(!game.is_win(), "Not win")
 		assert(game.to_right([muzhik, volk]), "Correct: [koza] --- []-[volk, muzhik, kapusta]")
+		assert(!game.is_win(), "Not win")
 		assert(game.to_left([muzhik]), "Correct: [koza, muzhik]-[] --- [volk, kapusta]")
+		assert(!game.is_win(), "Not win")
 		assert(game.to_right([muzhik, koza]), "Correct: [] --- []-[volk, koza, kapusta, muzhik")
+		assert(game.is_win(), "Win")
 	})()
+
+
+
+	var present_object_as_string = function(obj)
+	{
+		return obj.length > 1 ? obj[0] + "-" + obj[1] : obj[0]
+	}
+
+	var present_arr_as_string = function(arr)
+	{
+		return "[" + map(arr, present_object_as_string).join(", ") + "]"
+	}
+
+	var convert_string_to_object = function(str)
+	{
+		var matches = str.match(/^\W*?((?:\w|[а-я]|[А-Я])+).*?(\d*)\D*$/)
+		if(matches == null)
+			return null;
+
+		return matches[2].length > 0 ? [matches[1], matches[2]] : [matches[1]]
+	};
+
+	var present_game_as_string = function(game_raw)
+	{
+		var left = present_arr_as_string(game_raw.left)
+		var right = present_arr_as_string(game_raw.right)
+		var boat = present_arr_as_string(game_raw.boat)
+
+		return game_raw.boat_position == POS_LEFT
+			? left + " -- " + boat + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + right
+			: left + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + boat + " -- " + right
+	};
+
+	(function test_presenting_functions(){
+		assert("koza" == present_object_as_string(["koza"]), "Must be 'koza'")
+		assert("koza-1" == present_object_as_string(["koza", 1]), "Must be 'koza-1'")
+
+		var result = present_arr_as_string([["koza"], ["koza", 10]])
+		assert("[koza, koza-10]" == result,
+			"Waiting the '[koza, koza-10]' but was " + result)
+
+		var data = [
+			["  asdf23dsa23", 1, "asdf23dsa23", undefined],
+			["  asdf23dsa",   1, "asdf23dsa",   undefined],
+			["  asdf23dsa-4312njn-_jlk123jkj32fdsa", 2, "asdf23dsa", 32],
+			["  asdf23dsa-4312njn-_jlk123jkj32",     2, "asdf23dsa", 32],
+			["asdf23dsa-4312njn-_jlk123jkj32",       2, "asdf23dsa", 32],
+			["koza",    1, "koza", undefined],
+			["koza-10", 2, "koza", 10],
+			["koza-10", 2, "koza", 10],
+			["", null]
+		]
+
+		data.forEach(function(elem){
+			var result = convert_string_to_object(elem[0])
+			if(result != null)
+			{
+				assert(elem[1] == result.length, "Length of result must be " + elem[1])
+				assert(elem[2] == result[0], "Type of object must be " + elem[2])
+				assert(elem[3] == result[1], "Family of object must be " + elem[3])
+			}
+			else
+			{
+				assert(elem[1] == null, "Result must be null")
+			}
+		})
+	})()
+
+	var do_game_step = function(raw_game, method_name, args)
+	{
+		var what = []
+		Array.prototype.forEach.call(args, function(elem)
+		{
+			var parts = map(elem.split(","), convert_string_to_object)
+			what = what.concat(parts)
+		})
+
+		return raw_game[method_name](what)
+	}
+
+	var Game = function(game_raw)
+	{
+		this.game_raw = game_raw
+	}
+
+	Game.prototype.display_in_log = function()
+	{
+		console.log(present_game_as_string(this.game_raw));
+	}
+
+	Game.prototype.to_right = function()
+	{
+		return do_game_step(this.game_raw, "to_right", slice.call(arguments))
+	}
+	Game.prototype.to_left = function()
+	{
+		return do_game_step(this.game_raw, "to_left", slice.call(arguments))
+	};
+	Game.prototype.is_win = function()
+	{
+		return this.game_raw.is_win()
+	};
+
+	(function test_game_stiralka(){
+		var game_raw = new GameRaw({
+			left: [["muzhik", 1], ["muzhik", 2], ["muzhik"], ["stiralka"]],
+			boat_capacity: 3,
+			boat_rules: necessary(["muzhik"]),
+			transaction_rules: {stiralka: needs_at_least(["stiralka"], ["muzhik"], 3)}
+		});
+		var game = new Game(game_raw)
+		var present =  curry(present_game_as_string, game_raw);
+
+		assert(game.to_right("muzhik-1, muzhik-2", "stiralka"), "correct: [muzhik] --- [stiralka]-[muzhik-1, muzhik-2]")
+		assert(!game.is_win(), present)
+		assert(!game.to_right("muzhik"), "Incorrect boat side")
+		assert(!game.to_left(""), "Can't go without muzhik")
+		assert(game.to_left("muzhik-1"), "correct: [muzhik, muzhik-1]-[stiralka] --- [muzhik-2]")
+		assert(!game.is_win(), present)
+		assert(game.to_right("muzhik", "muzhik-1"), "correct: [] --- [] - [muzhik, muzhik-1, muzhik-2, stiralka]")
+		assert(game.is_win(), present)
+	})();
+
+	return {Game: Game, GameRaw: GameRaw}
 })()
-// var game = {
-// 	right: [["man"], ["volk"], ["koza"], ["kapusta"]],
-// 	left: [],
-// 	boat: [],
-// 	boat_position: "right",
-// 	rules:
-// }
 
 
+
+
+
+
+
+
+
+
+
+
+
+//==========================================================================
+
+//------- Globals. Game infrastructure ---------
+
+//==========================================================================
+
+var infrastructure = (function(){
+
+	var Game = game_contructors.Game
+	var GameRaw = game_contructors.GameRaw
+
+
+	var print = function(value) 
+	{
+		var args = slice.call(arguments, 1)
+		var type = typeof value
+		if(type == "function")
+		{
+			value.apply(this, args)
+		}
+		else if(type == "object" && value.constructor.toString().match("function Array.*"))
+		{
+			value.forEach(function(elem)
+			{
+				print.apply(this, [elem].concat(args))
+			})
+		}
+		else
+		{
+			console.log(value)
+			if(args.length > 0)
+			{
+				print.apply(this, args)
+			}
+		}
+	}
+
+	var print_help = function() {
+		print(commands.help.main)
+		print()
+	}
+
+	var print_commands = function(obj)
+	{
+		for(var i in obj)
+		{
+			if(obj.hasOwnProperty(i))
+			{
+				print(i + "\t\t" + obj[i].description)
+			}
+		}
+	}
+
+	var lesson = {
+		description : "Example lesson",
+		problems : [
+			{
+				title : "Volk, koza i kapusta",
+				description : ["What does the", "fox say"],
+				config : (function volk_koza_kapusta(){
+					var muzhik = ["muzhik"]
+					var volk = ["volk"]
+					var koza = ["koza"]
+					var kapusta = ["kapusta"]
+
+					var koza_volk = disabled(koza, volk)
+					var koza_kapusta = disabled(kapusta, koza)
+					var muzhik_koza = needs(koza, muzhik)
+					var muzhik_kapusta = needs(kapusta, muzhik)
+
+					return {
+						left: [volk, koza, kapusta, muzhik],
+						boat_capacity: 2,
+						rules: or(necessary(muzhik), and(koza_volk, koza_kapusta)),
+						boat_rules: necessary(muzhik)
+					}
+				})()
+			}
+		]
+	}
+
+	var current_problem_index;
+	var current_game;
+
+	var select_problem = function(index)
+	{
+		current_problem_index = index
+		current_game = new Game(new GameRaw(lesson.problems[index].config))
+	}
+	select_problem(0)
+
+	var commands = {
+		help : function(command)
+		{
+			if(!command)
+			{
+				print(commands.help.main)
+				print_commands(commands)
+			}
+			else
+			{
+				print(commands[command].full_description)
+			}
+		},
+
+		list : function()
+		{
+			print(lesson.description, "")
+			var len = lesson.problems.length
+			for(var i = 0; i < len; ++i)
+			{
+				print("№ " + i + ".\t" + lesson.problems[i].title)
+			}
+		},
+
+		show : function(index)
+		{
+			index = index === undefined ? current_problem_index : index
+			var problem = lesson.problems[index]
+
+			print("№ " + index + "." + problem.title + "\n")
+			print(problem.description)
+		},
+
+		start : function(index)
+		{
+			index = index === undefined ? current_problem_index : index
+			select_problem(index)
+		},
+
+		restart : function()
+		{
+			select_problem(current_problem_index)
+		},
+
+		state : function()
+		{
+			current_game.display_in_log()
+		},
+
+		to_left : function()
+		{
+			current_game.to_left.apply(current_game, arguments)
+			current_game.display_in_log()
+		},
+
+		to_right : function()
+		{
+			current_game.to_right.apply(current_game, arguments)
+			current_game.display_in_log()
+		}
+	}
+
+
+	var wrap = function(scope, property)
+	{
+		if(property == "help")
+			return scope[property]
+
+		return function(arg0)
+		{
+			if(arguments.length == 1 && (arg0 == "help" || arg0 == "?"))
+			{
+				scope.help(property)
+			}
+			else
+			{
+				scope[property].apply(scope, arguments)
+			}
+		}
+	}
+
+	var init_global_commands = function()
+	{
+		for(var i in commands)
+		{
+			if(commands.hasOwnProperty(i))
+			{
+				window[i] = wrap(commands, i)
+			}
+		}
+	}
+
+
+	var init_messages = function(messages) {
+
+		commands.help.main = messages.help_main
+
+		for(var i in commands)
+		{
+			if(commands.hasOwnProperty(i))
+			{
+				commands[i].description = messages[i]
+				commands[i].full_description = messages[i + "_full"]
+			}
+		}
+	}
+
+	var init_lesson = function(lesson1)
+	{
+		lesson = lesson1
+		select_problem(0)
+	}
+
+	return {
+		init_global_commands : init_global_commands,
+		init_messages :        init_messages,
+		init_lesson :          init_lesson
+	}
+})();
+
+(function(){
+	var help_messages = {
+		help : "help() - вывести эту справку; help('command') - вывести справку по конкретной команде",
+		help_main: "доступны следующие команды:\n",
+		help_full: [
+			"Команда help() с указанным в скобках и в кавычка названием другой команды выводит справку по этой команде",
+			"Примеры:",
+			"\thelp(\"list\")",
+			"\thelp(\"to_left\"",
+			"\thelp(\"help\")"],
+
+		list: "вывести список задач (№, название)",
+		list_full: ["Команда list() выводит список имеющихся задач. Вызывается без параметров.",
+			"Пример:", "\tlist()"],
+
+		show: "show(), show(number) - показать условие задачи",
+		show_full: ["Команда show() с указанным в скобках номером задачи выводит условие этой задачи",
+			"Вызыванная команда show() без аргументов показывает условие текущей задачи.",
+			"Примеры:",
+			"\tshow()\t - выведет условие текущей заадчи",
+			"\tshow(5)\t - выведет условие пятой заадчи"],
+
+		start: "start(number) - начать решать задачу с номером number",
+		start_full: ["Команда start() с указанным в скобках номером задачи стартует соответствующую задачу.",
+			"Вы её решаете, перемещая героев с берега на берег при помощи команд to_left() и to_right()",
+			"Пример:", "\tstart(5)\t - стартуем пятую задачу"],
+
+		restart: "начать решать текущую задачу заново",
+		restart_full: ["Команда restart() сбрасывает все ваши действия и вы начинаете решать задачу заново. Команда restart() вызывается без аргументов"],
+
+		state: "отобразить состояние текущей задачи",
+		state_full: ["Команда state() вызывается без аргументов и отображает состояние текущей игры. Показывает, где какой участник расположен: на лодке или на одном из берегов.",
+			"Формат отображения следующий:",
+			"[левый берег] -- [лодка] ~~~~~~~~~~~~~~ [правый берег]\t - если лодка на левом берегу",
+			"[левый берег] ~~~~~~~~~~~~~~ [лодка] -- [правый берег]\t - если лодка на правом берегу",
+			"Примеры:",
+			"\tstate() \t -> \t [волк, капуста] ~~~~~~~~~~~~~~ [] -- [коза, лодочник]"],
+
+		to_left: "to_left('volk, koza, kapusta') - отправить перечисленных участников на левый берег",
+		to_left_full: "to_left('volk, koza, kapusta') - отправить перечисленных участников на левый берег",
+		to_right: "to_right('chemodan-1, man-1') - отправить перечисленных участников на правый берег",
+		to_right_full: "to_right('volk, koza, kapusta') - отправить перечисленных участников на правый берег"
+	}
+
+
+	infrastructure.init_messages(help_messages)
+})();
+
+
+
+
+//==========================================================================
+
+//------- Problems ---------
+
+//==========================================================================
+(function(){
+	var lesson = {
+		title : "Переправы от Шаповаловых",
+		description : ["Все знают задачу про перевозку волка, козы и капусты. В первый раз она производит большое впечатление. Ведь казалось, что невозможно, и вдруг - вот способ! И всё так наглядно: можно взять фигурки и подвигать их, а то и поиграть: тф будешь козой, ты волком, ты крестянином...",
+				"Подборки задач на переправу пользуются популярностью. Но вот список сюжетов в них очень небольшой: два мальчика и полк солдат, рыцари и оруженосцы (или дамы сердца), миссионеры и каннибалы (или купцы и разбойники), переход через мост с фонариком. Для каждого сюжета есть 2-3 задачи. Мало!",
+				"Тогда я решил придумать сам. С первой задачей помог сын, вспомнив, как мы переезжали на новую квартиру и грузчиков не хватало. Оказалось, что в такие задачи можно \"зашить\" математику и посерьёзнее. Порешайте и убедитесь в этом сами. Только давайте сразу договоримся: никаких нематематических трюков с выпрыгиванием из лодки, которая не пристала к берегу. А лодку, приставшую к берегу, будем считать частью этого берега."],
+
+		problems : [
+			{
+				title : "Волк, коза и капуста",
+				description : ["Классическая задача. Если лодочник уплыл, то волк ест козу, или коза ест капусту. В лодку помещается не более двух персонажей, а грести умеет только лодочник. Как всем переправиться?",
+					"Командой state() вы отображаете текущее состояние.",
+					"Командами to_right(), to_left() возите героев туда-сюда",
+					"Пример:",
+					"\tto_right(\"лодочник, волк\")"],
+				config : (function volk_koza_kapusta(){
+					var muzhik = ["лодочник"]
+					var volk = ["волк"]
+					var koza = ["коза"]
+					var kapusta = ["капуста"]
+
+					var koza_volk = disabled(koza, volk)
+					var koza_kapusta = disabled(kapusta, koza)
+					var muzhik_koza = needs(koza, muzhik)
+					var muzhik_kapusta = needs(kapusta, muzhik)
+
+					return {
+						left: [volk, koza, kapusta, muzhik],
+						boat_capacity: 2,
+						rules: or(necessary(muzhik), and(koza_volk, koza_kapusta)),
+						boat_rules: necessary(muzhik)
+					}
+				})()
+			}
+		]
+	}
+
+	infrastructure.init_lesson(lesson)
+})()
+
+
+
+
+	infrastructure.init_global_commands();
 
 
 })()
