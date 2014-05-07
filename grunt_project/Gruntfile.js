056ab8c415636a9bb07c7e913cfac68e6596cc9c
chivorotkiv <shmakov@omich.net>
2014-05-08 06:48:40 +0700

    [d-136] Ignore what must be ignored.

--- grunt_project/Gruntfile.js
+++ grunt_project/Gruntfile.js
@@ -107,6 +107,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'watch']);
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 
 };
