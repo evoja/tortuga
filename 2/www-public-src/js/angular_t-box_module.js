463c9f9095a23c48a5b86927632dc74ac63d6eb3
chivorotkiv <shmakov@omich.net>
2014-05-09 22:54:16 +0700

    [d-139] Tried to make module configuration more readable. Use ns_get everywhere to get constructors. In that case we always get a message of unexacting object instead of getting undefined is not a function somewhere in angular.

--- 2/www-public-src/js/angular_t-box_module.js
+++ 2/www-public-src/js/angular_t-box_module.js
@@ -1,22 +1,46 @@
 (function() {
+var ns_get = Om.ns_get;
+var curry = ns_get("Om.curry");
 
-var curry = Om.ns_get("Om.curry");
+var tbox_get = function(postfix){return ns_get("Trtg.TBox." + postfix)};
+var tang_get = function(postfix){return ns_get("Trtg.TBox.Ang." + postfix)};
 
-angular.module('t_box_module.TBoxTortoiseCanvas', [])
-    .value('TBoxTortoiseCanvasBlock', Om.ns_get("Trtg.TBox.TBlocks.TortoiseCanvasBlock"))
-    .service('TBoxTortoiseCanvasService', ['TBoxTortoiseCanvasBlock', Om.ns_get("Trtg.TBox.Ang.MethodsDispatcherService")])
-    .controller('TBoxTortoiseCanvasController', ['$scope', 'TBoxTortoiseCanvasService', Om.ns_get("Trtg.TBox.Ang.ServiceProxyController")])
-    .directive('tboxTortoiseCanvas', curry(Trtg.TBox.Ang.TBlocks.TortoiseCanvasDirective, 'TBoxTortoiseCanvasController'))
+
+angular.module("t_box_module.TBoxTortoiseCanvas", [])
+    .value("TBoxTortoiseCanvasBlock",
+                 tbox_get("TBlocks.TortoiseCanvasBlock"))
+
+    .service("TBoxTortoiseCanvasService",
+                ["TBoxTortoiseCanvasBlock", tang_get("MethodsDispatcherService")])
+
+    .controller("TBoxTortoiseCanvasController",
+                ["$scope", "TBoxTortoiseCanvasService", tang_get("ServiceProxyController")])
+
+    .directive("tboxTortoiseCanvas",
+                curry(tang_get("TBlocks.TortoiseCanvasDirective"), "TBoxTortoiseCanvasController"))
     ;
-angular.module('t_box_module', ['t_box_module.TBoxTortoiseCanvas'])
-    .service('DispatcherService', Trtg.TBox.Ang.DispatcherService)
-    .controller('DispatcherController', ['$scope', 'DispatcherService', Trtg.TBox.Ang.DispatcherController])
-    .directive('consoleOut', curry(Trtg.TBox.Ang.ConsoleOutDirective, 'DispatcherController'))
-    .directive('consoleIn', curry(Trtg.TBox.Ang.ConsoleInDirective, 'DispatcherController'))
-    ;
-angular.bootstrap(document.getElementById('t_box_module'), ['t_box_module']);
 
-angular.module('other', []);
-angular.bootstrap(document.getElementById('other_module'), ['other']);
+
+angular.module("t_box_module", ["t_box_module.TBoxTortoiseCanvas"])
+    .service("DispatcherService",
+                tang_get("DispatcherService"))
+
+    .controller("DispatcherController",
+                ["$scope", "DispatcherService", tang_get("DispatcherController")])
+
+    .directive("consoleOut",
+                curry(tang_get("ConsoleOutDirective"), "DispatcherController"))
+
+    .directive("consoleIn",
+                curry(tang_get("ConsoleInDirective"), "DispatcherController"))
+    ;
+
+angular.bootstrap(document.getElementById("t_box_module"), ["t_box_module"]);
+
+
+
+
+angular.module("other", []);
+angular.bootstrap(document.getElementById("other_module"), ["other"]);
 
 })();
